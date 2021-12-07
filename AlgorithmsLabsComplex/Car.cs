ffe5a31f34d0c73724cab72f4b01c09da0a741ce
Макс Дороболюк <dmv_01@mail.ru>
2021-12-07 23:50:38 +0600

    1

---
+++ AlgorithmsLabsComplex/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public string Number { get; set; }
+        public override string ToString()
+        {
+            return Model+Number;
+        }
+    }
+}
--- AlgorithmsLabsComplex/Program.cs
+++ AlgorithmsLabsComplex/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using Collections;
+using Lab2;
+using System;
 
 namespace AlgorithmsLabsComplex
 {
     class Program
     {
+        static string[] models = new string[] { "BMW", "Lada", "Jaguar" };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random(2);
+            BinaryTree<Car> tree = new Collections.BinaryTree<Car>();
+            tree.sortFunc = x =>
+             {
+                 if (double.TryParse(x.Number, out double r))
+                     return r;
+                 else
+                     return 999999;
+             };
+            for (int i = 0; i < 6; i++)
+            {
+                tree.PutItem(new Car()
+                {
+                    Number = rnd.Next(1000).ToString(),
+                    Model = models[rnd.Next(models.Length)]
+                });
+            }
+            tree.ShowInDebug();
+            Console.ReadLine();
+            for (int i = 0; i < 6; i++)
+            {
+                tree.PutItem(new Car()
+                {
+                    Number = rnd.Next(1000).ToString(),
+                    Model = models[rnd.Next(models.Length)]
+                });
+            }
+
+            tree.ShowInDebug();
+            Console.ReadLine();
+
+            //tree.OrderTreeBy(x =>
+            //{
+            //    if (double.TryParse(x.Number, out double r))
+            //        return r;
+            //    else 
+            //        return 999999;
+            //});
+            tree.ShowInDebug();
+
+            
         }
     }
 }
